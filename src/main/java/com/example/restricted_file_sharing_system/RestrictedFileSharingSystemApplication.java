a072c29e6b0ee4835880371864795c9a0f755f25
Shuvo Chandra Deb Nath <shuvochandra999@gmail.com>
2026-02-04 13:19:28 +0600

    Project Initialized

---
+++ src/main/java/com/example/restricted_file_sharing_system/RestrictedFileSharingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.example.restricted_file_sharing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestrictedFileSharingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestrictedFileSharingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/restricted_file_sharing_system/RestrictedFileSharingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.restricted_file_sharing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestrictedFileSharingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
